9f63b3e9e5f7e8ba56f7d5d35d96f7ee4ca9612d
Daniel A Cummings <danielac@protonmail.com>
2026-06-13 16:42:01 -0600

    Display a Party Pack of 1 string from each category in a TOML file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from party_pack import get_party_pack, PartyPack
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to PartyPackApi!"}
+    return {"message": "Future, simple UI"}
+
+@app.get("/party-pack", response_model=PartyPack)
+def party_pack_endpoint():
+    return get_party_pack()
